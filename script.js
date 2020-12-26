7cb4dd97c4f727a35b642f1fa74a293d6bfa799a
schap <schap@Beni-Notebook.fritz.box>
2020-12-26 18:59:37 +0100

    1 prototyp

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 /*Das ist ein Kommentar*/
+
+function printDiv(x) {
+     var printContents = document.getElementById(x).innerHTML;
+     var originalContents = document.body.innerHTML;
+
+     document.body.innerHTML = printContents;
+     window.print();
+     document.body.innerHTML = originalContents;
+}
